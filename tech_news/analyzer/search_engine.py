c2da0f7090b60865bbc85aec1e1ca85ef8fa7020
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-14 00:03:52 -0300

    feat: adds search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,17 +1,25 @@
 from tech_news.database import search_news
 import re
+from datetime import datetime
 
 
 # Requisito 7
 def search_by_title(title):
-    compiled_regex = re.compile(rf'{title}', re.I)
+    compiled_regex = re.compile(rf"{title}", re.I)
     found_results = search_news({"title": {"$regex": compiled_regex}})
     return [(result["title"], result["url"]) for result in found_results]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        converted_date = datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        found_results = search_news({"timestamp": {"$regex": converted_date}})
+        return [(result["title"], result["url"]) for result in found_results]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
